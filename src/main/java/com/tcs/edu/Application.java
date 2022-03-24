ee77d077310f8960f68e5229d7a1624ffce8b2a3
BasovaElvira <elarabocij@gmail.com>
2022-03-24 13:22:51 +0300

    add packages

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,3 +1,7 @@
+package com.tcs.edu;
+
+import com.tcs.edu.printer.ConsolePrinter;
+
 public class Application {
     public static void main(String[] args) {
        ConsolePrinter.print("Hello world!");
