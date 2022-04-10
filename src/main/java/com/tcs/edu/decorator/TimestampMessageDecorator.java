aba7cb7db2975e55b86f9d7fdec915ddbd8093fe
e.basova <e.basova@tinkoff.ru>
2022-04-10 22:31:22 +0300

    add separation to message

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -9,13 +9,26 @@ import java.time.Instant;
  */
 
 public class TimestampMessageDecorator {
+
+    static final Integer PAGE_SIZE = 2;
     static int messageCount = 1;
+
+    public static int getMessageCount() {
+        return messageCount;
+    }
     /**
-     * Method decorate is adding the time to message
+     * Method decorate is adding the time and a separation to message
      * Side effect: message is changed by the time
      * @param message needed text
      */
     public static String decorate(String message) {
+
+        if (messageCount % PAGE_SIZE == 0) {
+            final var decoratedMessage = String.format("%d %s %s\n---", messageCount, Instant.now(), message);
+            messageCount++;
+            return decoratedMessage;
+        }
+
         final var decoratedMessage = String.format("%d %s %s", messageCount, Instant.now(), message);
         messageCount++;
         return decoratedMessage;
