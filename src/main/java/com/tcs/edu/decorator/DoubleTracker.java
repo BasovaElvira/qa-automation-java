b214ea76cff21a9e3a1c33c599541b076a23d83f
e.basova <e.basova@tinkoff.ru>
2022-05-09 21:53:00 +0300

    hw 8: add class Message, refactor

--- src/main/java/com/tcs/edu/decorator/DoubleTracker.java
+++ src/main/java/com/tcs/edu/decorator/DoubleTracker.java
@@ -1,16 +1,40 @@
 package com.tcs.edu.decorator;
 
-import java.util.stream.Stream;
+import com.tcs.edu.domain.*;
 
-import static java.util.Arrays.stream;
+public interface DoubleTracker {
 
-public class DoubleTracker {
-    public static Stream<String> checkDoubles (Doubling doubling, String... messages) {
-        var type = stream(messages);
-
-        if(doubling == Doubling.DISTINCT) {
-            type = type.distinct();
+    /**
+     * @param messages array of Message
+     * @return delete doubles in messages
+     */
+    public static Message[] deleteDoubles (Doubling doubling, Message... messages) {
+        Message[] messagesOutput = new Message[messages.length];
+        if (messages.length != 0) {
+            messagesOutput[0] = messages[0];
+            int k = 1;
+            for (int i = 1; i < messages.length; i++) {
+                if (!checkDoubles(messages[i], messagesOutput)) {
+                    messagesOutput[k] = messages[i];
+                    k++;
+                }
+            }
         }
-        return type;
+        return messagesOutput;
+    }
+
+    /**
+     * @param messages array of Message
+     * @return looking for doubles in messages
+     */
+    public static boolean checkDoubles(Message message, Message... messages) {
+        if (message != null) {
+            for (Message inMessage : messages) {
+                if (inMessage != null && inMessage.getMessage().equals(message.getMessage())) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/main/java/com/tcs/edu/decorator/MessageCountDecorator.java
+++ src/main/java/com/tcs/edu/decorator/MessageCountDecorator.java
@@ -6,6 +6,9 @@ public class MessageCountDecorator {
 
     public static Integer messageCount = 0;
 
+    /**
+     * @return add count to message
+     */
     public static Integer addCount() {
         ++messageCount;
         return  messageCount;
