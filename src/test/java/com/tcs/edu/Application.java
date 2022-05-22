2c7e9560d8ac6f195490e41870b146b8c529dcb0
e.basova <e.basova@tinkoff.ru>
2022-05-22 15:09:44 +0300

    hw 9: add exception for null

--- src/test/java/com/tcs/edu/Application.java
+++ src/test/java/com/tcs/edu/Application.java
@@ -10,9 +10,9 @@ public class Application {
     public static void main(String[] args) {
 
         new DecoratorMessageService().log(
+                new Message(),
                 new Message(null),
-                new Message(null),
-                new Message("Hello11"),
+                new Message(""),
                 new Message("Hello11")
         );
 
